16ce04357c75b96751b79558971692ead8e6a25c
HELMEN Anders <anders.helmen@soprasteria.com>
2020-03-11 16:24:15 +0100

    Add needed packages and use boolean for testing part of class

--- manifests/install.pp
+++ manifests/install.pp
@@ -13,37 +13,56 @@ class netbox::install (
   Stdlib::Absolutepath $download_tmp_dir,
   String $user,
   String $group,
+  Boolean $included = true
 ) {
 
-  $local_tarball = "${download_tmp_dir}/netbox-${version}.tar.gz"
-  $software_directory = "${install_root}/netbox-${version}"
+  $packages =[
+    gcc,
+    python36,
+    python36-devel,
+    libxml2-devel,
+    libxslt-devel,
+    libxslt-devel,
+    libffi-devel,
+    openssl-devel,
+    redhat-rpm-config
+  ]
 
-  archive { $local_tarball:
-    source        => $download_url,
-    checksum      => $download_checksum,
-    checksum_type => $download_checksum_type,
-    extract       => true,
-    extract_path  => $install_root,
-    creates       => $software_directory,
-    cleanup       => true,
-    user          => $user,
-    group         => $group,
+  if $included {
+    package { $packages: ensure => 'installed' }
   }
+  else {
+    $local_tarball = "${download_tmp_dir}/netbox-${version}.tar.gz"
+    $software_directory = "${install_root}/netbox-${version}"
 
-  user { $user:
-    system => true,
-    gid    => $group,
-    home   => $install_root,
-  }
+    archive { $local_tarball:
+      source        => $download_url,
+      checksum      => $download_checksum,
+      checksum_type => $download_checksum_type,
+      extract       => true,
+      extract_path  => $install_root,
+      creates       => $software_directory,
+      cleanup       => true,
+      user          => $user,
+      group         => $group,
+    }
 
-  group { $group:
-    system => true,
-  }
+    user { $user:
+      system => true,
+      gid    => $group,
+      home   => $install_root,
+    }
 
-  file { $install_root:
-    ensure => directory,
-    owner  => 'netbox',
-    group  => 'netbox',
-    mode   => '0750',
+    group { $group:
+      system => true,
+    }
+
+    file { $install_root:
+      ensure => directory,
+      owner  => 'netbox',
+      group  => 'netbox',
+      mode   => '0750',
+    }
   }
 }
+
